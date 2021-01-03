f3d6fd9bef4c741d3edd66e228a0f878c6b201f9
anas11cs <annus.waheed01@gmail.com>
2021-01-03 20:48:57 +0500

    Final Commit: Configured MongoDb,Mongoose, Clear All Logs functionality and Released App

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 const path = require('path')
 const url = require('url')
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
 const connectDB = require('./config/db')
 const Log = require('./models/Log')
 let mainWindow
 let isDev = false
+
+const isMac = process.platform === 'darwin'? true : false
+
 if (
 	process.env.NODE_ENV !== undefined &&
 	process.env.NODE_ENV === 'development'
@@ -66,8 +69,110 @@ function createMainWindow() {
 	mainWindow.on('closed', () => (mainWindow = null))
 }
 
-app.on('ready', createMainWindow)
+app.on('ready', () =>{
+	createMainWindow() // 1st Render THING on Window
+	const mainMenu=Menu.buildFromTemplate(menu) // 2nd Render THING on Window
+	Menu.setApplicationMenu(mainMenu)
+})
 
+// ==================================== APP MENU BELOW  ====================================
+
+
+const menu = [
+	...(isMac ? [{ role: 'appMenu' }] : []),
+	{
+	  role: 'fileMenu',
+	},
+	{
+	  role: 'editMenu',
+	},
+	{
+	  label: 'Logs',
+	  submenu: [
+		{
+		  label: 'Clear Logs',
+		  click: () => clearLogs(),
+		},
+	  ],
+	},
+	...(isDev
+	  ? [
+		  {
+			label: 'Developer',
+			submenu: [
+			  { role: 'reload' },
+			  { role: 'forcereload' },
+			  { type: 'separator' },
+			  { role: 'toggledevtools' },
+			],
+		  },
+		]
+	  : []),
+  ]
+
+// ====================================  ==================================== IPC PROCESSES BELOW  ====================================  ====================================
+// Helper Functions Below
+// Send Logs & Clear Logs
+async function sendLogs()// Function to fetch data from data base
+{
+	try{
+		const logs = await Log.find().sort({created:1/*1=Ascending*/})
+		mainWindow.webContents.send('logs:get', JSON.stringify(logs))
+	}
+	catch(err){
+		console.log(err)
+	}
+}
+async function clearLogs()// Function to fetch data from data base
+{
+	try{
+		const logs = await Log.deleteMany({})
+		mainWindow.webContents.send('logs:clear'/*, JSON.stringify(logs)*/) // Second Parameter Removed as nothing to render, since all logs are cleared
+	}
+	catch(err){
+		console.log(err)
+	}
+}
+ // ====================================
+ // ====================================
+// Catching the sent logs here. Sent from App.js by ipcRenderer.send('logs:load')
+// Load Log
+ipcMain.on('logs:load',sendLogs)
+//Create Log
+ipcMain.on('logs:add', async (e,item) => {
+	try{
+		await Log.create(item)
+		sendLogs()
+		//console.log('here')
+	} catch(err){
+		console.log(err)
+	}
+	//console.log(item)
+})
+ // ====================================
+//Delete Log
+ipcMain.on('logs:delete', async (e,id) => {
+	try{
+		await Log.findOneAndDelete({ _id:id })
+		sendLogs()
+		//console.log('here')
+	} catch(err){
+		console.log(err)
+	}
+	//console.log(item)
+})
+ // ====================================
+// Clear Log
+ ipcMain.on('logs:clear', async(e) => {
+	 try{
+
+	 }
+	 catch(err){
+		console.log(err)
+	 }
+ })
+ // ====================================
+// ====================================
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
 		app.quit()
--- src/components/App.js
+++ src/components/App.js
@@ -4,47 +4,61 @@ import Container from 'react-bootstrap/Container'
 import LogItem from './LogItem'
 import AddLogItem from './AddLogItem'
 import Alert from 'react-bootstrap/Alert'
+import { ipcRenderer } from 'electron'
+
 const App = () => {
 	const [logs, setLogs] = useState([
-		{
-			_id:1,
-			text:'This is log One',
-			priority: 'moderate',
-			user:'Kate',
-			created: new Date().toString(),
-		},
-		{
-			_id:2,
-			text:'This is log two',
-			priority: 'low',
-			user:'Brad',
-			created: new Date().toString(),
-		},
-		{
-			_id:3,
-			text:'This is log Three',
-			priority: 'high',
-			user:'John',
-			created: new Date().toString(),
-		},
+		// {
+		// 	_id:1,
+		// 	text:'This is log One',
+		// 	priority: 'moderate',
+		// 	user:'Kate',
+		// 	created: new Date().toString(),
+		// },
+		// {
+		// 	_id:2,
+		// 	text:'This is log two',
+		// 	priority: 'low',
+		// 	user:'Brad',
+		// 	created: new Date().toString(),
+		// },
+		// {
+		// 	_id:3,
+		// 	text:'This is log Three',
+		// 	priority: 'high',
+		// 	user:'John',
+		// 	created: new Date().toString(),
+		// },
 	])
 	const [alert, setAlert] = useState({
 		show:false, // When we want to show Alert Message, we put it to true
 		message:'',
 		variant: 'success'
 	})
-	
+	useEffect(() => {
+		ipcRenderer.send('logs:load') // <= This is to main process know that logs are ready to load
+		ipcRenderer.on('logs:get',(e,logs) => {
+			setLogs(JSON.parse(logs))
+		})
+		ipcRenderer.on('logs:clear',() => {
+			setLogs([])
+			showAlert('Logs Cleared !')
+		})
+	},[]/*Empty array as no dependency, if there is depedency then we send it here*/)
 	// Adding Element in the above prototype data
 	function addItem(item){
-		item._id= Math.floor(Math.random()*90000)+10000
-		item.created=new Date().toString()
-		if(item.text===null || item.user === null || item.priority === null)
+		if(item.text==='' || item.user === '' || item.priority === '')
         {
+			showAlert('Please Enter All Fields','danger')
+			return false;
           //Alert()
           console.log('Masti Kar raya aen')
         }
-		/*...logs means all the logs from exisiting arrau */
-		setLogs([...logs, item])
+		// item._id= Math.floor(Math.random()*90000)+10000
+		// item.created=new Date().toString()
+		/*...logs means all the logs from exisiting array */
+		// setLogs([...logs, item])
+		ipcRenderer.send('logs:add', item)
 		showAlert('Log Added !')
 	}
 	function showAlert(message, variant='success', seconds=3000)
@@ -63,9 +77,10 @@ const App = () => {
 		}, seconds)
 	}
 	// Deleting Item from the above Logs
-	function deleteItem(id)
+	function deleteItem(_id)
 	{
-		setLogs(logs.filter((item) => item._id !== id))
+		//setLogs(logs.filter((item) => item._id !== id))
+		ipcRenderer.send('logs:delete', _id)
 		showAlert('Log Removed !')
 	}
 	// The part Below is Rendered on the Screen
